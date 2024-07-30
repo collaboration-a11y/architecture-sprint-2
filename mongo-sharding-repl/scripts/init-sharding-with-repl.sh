2de60049ee07b97523d4b4179747b9fe1b6e59e6
ereshidov <newfaceof+2@yandex.ru>
2024-07-31 02:59:22 +0300

    feat: sharding with replication for each shard

--- mongo-sharding-repl/scripts/init-sharding-with-repl.sh
+++ mongo-sharding-repl/scripts/init-sharding-with-repl.sh
@@ -11,16 +11,19 @@ rs.initiate({ _id: "config_server", configsvr: true, members: [ { _id: 0, host: 
 exit();
 EOF
 
-# Инициализируем реплики для каждого шарда
+# Инициализируем реплики
 
-echo "Инициализируем поочердно реплики для каждого шарда"
+echo "Инициализируем поочердно реплики"
 
 docker compose exec -T shard1 mongosh --port $SH_PORT_1 <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:$SH_PORT_1" },
+        { _id: 0, host: "shard1:$SH_PORT_1" },
+        { _id: 2, host: "shard1_mongodb_replica1:27025" },
+        { _id: 3, host: "shard1_mongodb_replica2:27026" },
+        { _id: 4, host: "shard1_mongodb_replica3:27027" },
       ]
     }
 );
@@ -32,7 +35,10 @@ rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 1, host : "shard2:$SH_PORT_2" },
+        { _id: 1, host : "shard2:$SH_PORT_2" },
+        { _id: 5, host: "shard2_mongodb_replica1:27028" },
+        { _id: 6, host: "shard2_mongodb_replica2:27029" },
+        { _id: 7, host: "shard2_mongodb_replica3:27030" },
       ]
     }
 );
@@ -75,4 +81,33 @@ use $DATABASE_NAME
 db.helloDoc.countDocuments()
 exit();
 EOF
+
+# echo "Выводим количество документов в первом шарде в каждой  из созданных реплик (Ожидается 492 документа)"
+
+shard1ReplicaPorts=("27025", "27026", "27027")
+
+for replicaPort in ${shard1ReplicaPorts[@]} 
+do
+  docker compose exec -T shard1 mongosh --port $SH_PORT_1 <<EOF
+  use $DATABASE_NAME
+  db.getMongo().setReadPref('secondary')
+  db.helloDoc.countDocuments();
+  exit();
+EOF
+done
+
+shard2ReplicaPorts=("27028", "27029", "27030")
+
+for replicaPort in ${shard2ReplicaPorts[@]} 
+do
+  docker compose exec -T shard2 mongosh --port $SH_PORT_2 <<EOF
+  use $DATABASE_NAME
+  db.getMongo().setReadPref('secondary')
+  db.helloDoc.countDocuments();
+  exit();
+EOF
+done
+
+
+
 
