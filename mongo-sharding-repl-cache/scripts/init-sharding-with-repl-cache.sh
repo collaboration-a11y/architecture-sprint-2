5faf5ec0edb3cbc95043effc45fb0dcbfaae7c29
ereshidov <newfaceof+2@yandex.ru>
2024-07-31 05:59:33 +0300

    fix: remove redundant env variables

--- mongo-sharding-repl-cache/scripts/init-sharding-with-repl-cache.sh
+++ mongo-sharding-repl-cache/scripts/init-sharding-with-repl-cache.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-source .env
-
 # Инициализация конфигурационного сервера
 
 echo "Инициализируем конфигурационный сервер"
 
-docker compose exec -T configSvr mongosh --port $CONFIG_SVR_PORT <<EOF
-rs.initiate({ _id: "config_server", configsvr: true, members: [ { _id: 0, host: "configSvr:$CONFIG_SVR_PORT" } ]});
+docker compose exec -T configSvr mongosh --port 27016 <<EOF
+rs.initiate({ _id: "config_server", configsvr: true, members: [ { _id: 0, host: "configSvr:27016" } ]});
 exit();
 EOF
 
@@ -15,12 +13,12 @@ EOF
 
 echo "Инициализируем поочердно реплики"
 
-docker compose exec -T shard1 mongosh --port $SH_PORT_1 <<EOF
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id: 0, host: "shard1:$SH_PORT_1" },
+        { _id: 0, host: "shard1:27018" },
         { _id: 2, host: "shard1_mongodb_replica1:27025" },
         { _id: 3, host: "shard1_mongodb_replica2:27026" },
         { _id: 4, host: "shard1_mongodb_replica3:27027" },
@@ -30,12 +28,12 @@ rs.initiate(
 exit();
 EOF
 
-docker compose exec -T shard2 mongosh --port $SH_PORT_2 <<EOF
+docker compose exec -T shard2 mongosh --port 27019 <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id: 1, host : "shard2:$SH_PORT_2" },
+        { _id: 1, host : "shard2:27019" },
         { _id: 5, host: "shard2_mongodb_replica1:27028" },
         { _id: 6, host: "shard2_mongodb_replica2:27029" },
         { _id: 7, host: "shard2_mongodb_replica3:27030" },
@@ -48,9 +46,9 @@ EOF
 
 echo "Добавляем созданные шарды в настройку роутера и включаем шардирование"
 
-docker compose exec -T mongos_router mongosh --port $MONGOS_ROUTER_PORT <<EOF
-sh.addShard("shard1/shard1:$SH_PORT_1");
-sh.addShard("shard2/shard2:$SH_PORT_2");
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard("shard1/shard1:27018");
+sh.addShard("shard2/shard2:27019");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
 use somedb
@@ -68,16 +66,16 @@ EOF
 
 echo "Выводим количество документов в каждом из шардов"
 
-docker compose exec -T shard1 mongosh --port $SH_PORT_1 <<EOF
-use $DATABASE_NAME
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
+use somedb
 db.helloDoc.countDocuments()
 exit();
 EOF
 
 echo "Выводим количество документов в каждом из шардов"
 
-docker compose exec -T shard2 mongosh --port $SH_PORT_2 <<EOF
-use $DATABASE_NAME
+docker compose exec -T shard2 mongosh --port 27019 <<EOF
+use somedb
 db.helloDoc.countDocuments()
 exit();
 EOF
@@ -88,8 +86,8 @@ shard1ReplicaPorts=("27025", "27026", "27027")
 
 for replicaPort in ${shard1ReplicaPorts[@]} 
 do
-  docker compose exec -T shard1 mongosh --port $SH_PORT_1 <<EOF
-  use $DATABASE_NAME
+  docker compose exec -T shard1 mongosh --port 27018 <<EOF
+  use somedb
   db.getMongo().setReadPref('secondary')
   db.helloDoc.countDocuments();
   exit();
@@ -100,8 +98,8 @@ shard2ReplicaPorts=("27028", "27029", "27030")
 
 for replicaPort in ${shard2ReplicaPorts[@]} 
 do
-  docker compose exec -T shard2 mongosh --port $SH_PORT_2 <<EOF
-  use $DATABASE_NAME
+  docker compose exec -T shard2 mongosh --port 27019 <<EOF
+  use somedb
   db.getMongo().setReadPref('secondary')
   db.helloDoc.countDocuments();
   exit();
